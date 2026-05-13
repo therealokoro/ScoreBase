aa5d7258280e13e3fe7f6d1b8826a04369279841
Okoro Redemption <okororedemption24@gmail.com>
2026-05-13 14:15:13 +0100

    chore: installed server dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "latest",
+  devtools: { enabled: true },
+  modules: ["@nuxthub/core"],
+  hub: {
+    kv: true,
+    db: { dialect: "sqlite", casing: "snake_case" },
+  },
+  vite: {
+    // plugins: [tailwindcss()],
+    optimizeDeps: {
+      noDiscovery: true,
+      include: [
+        "@orpc/client",
+        "@orpc/client/fetch",
+        "@orpc/client/plugins",
+        "@orpc/tanstack-query",
+        "@tanstack/vue-query",
+        "class-variance-authority",
+        "reka-ui",
+        "tailwind-variants",
+        "clsx",
+        "tailwind-merge",
+      ],
+    },
+  },
+  runtimeConfig: {
+    betterAuthSecret: process.env.NUXT_BETTER_AUTH_SECRET,
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL,
+    },
+  },
+});
